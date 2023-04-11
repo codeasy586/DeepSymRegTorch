ef8708aeef1b03b73ffcc2d18a107d74b8799675
Samaksh Chandra <64631239+codeasy586@users.noreply.github.com>
2023-04-11 18:07:08 +0100

    Update benchmark_l0.py

--- benchmark_l0.py
+++ benchmark_l0.py
@@ -294,19 +294,19 @@ if __name__ == "__main__":
 
     bench = Benchmark(**kwargs)
 
-    bench.benchmark(lambda x: x, func_name="x", trials=10)
-    # bench.benchmark(lambda x: x**2, func_name="x^2", trials=20)
-    # bench.benchmark(lambda x: x**3, func_name="x^3", trials=20)
-    # bench.benchmark(lambda x: np.sin(2*np.pi*x), func_name="sin(2pix)", trials=20)
-    # bench.benchmark(lambda x: np.exp(x), func_name="e^x", trials=20)
-    # bench.benchmark(lambda x, y: x*y, func_name="xy", trials=20)
-    # bench.benchmark(lambda x, y: np.sin(2 * np.pi * x) + np.sin(4*np.pi * y),
-    #                 func_name="sin(2pix)+sin(4py)", trials=20)
-    # bench.benchmark(lambda x, y, z: 0.5*x*y + 0.5*z, func_name="0.5xy+0.5z", trials=20)
-    # bench.benchmark(lambda x, y, z: x**2 + y - 2*z, func_name="x^2+y-2z", trials=20)
-    # bench.benchmark(lambda x: np.exp(-x**2), func_name="e^-x^2", trials=20)
-    # bench.benchmark(lambda x: 1 / (1 + np.exp(-10*x)), func_name="sigmoid(10x)", trials=20)
-    # bench.benchmark(lambda x, y: x**2 + np.sin(2*np.pi*y), func_name="x^2+sin(2piy)", trials=20)
+    bench.benchmark(lambda x: x, func_name="x", trials=5)
+    bench.benchmark(lambda x: x**2, func_name="x^2", trials=20)
+    bench.benchmark(lambda x: x**3, func_name="x^3", trials=20)
+    bench.benchmark(lambda x: np.sin(2*np.pi*x), func_name="sin(2pix)", trials=20)
+    bench.benchmark(lambda x: np.exp(x), func_name="e^x", trials=20)
+    bench.benchmark(lambda x, y: x*y, func_name="xy", trials=5)
+    bench.benchmark(lambda x, y: np.sin(2 * np.pi * x) + np.sin(4*np.pi * y),
+                    func_name="sin(2pix)+sin(2py)", trials=20)
+    bench.benchmark(lambda x, y, z: 0.5*x*y + 0.5*z, func_name="0.5xy+0.5z", trials=5)
+    bench.benchmark(lambda x, y, z: x**2 + y - 2*z, func_name="x^2+y-2z", trials=20)
+    bench.benchmark(lambda x: np.exp(-x**2), func_name="e^-x^2", trials=20)
+    bench.benchmark(lambda x: 1 / (1 + np.exp(-10*x)), func_name="sigmoid(10x)", trials=20)
+    bench.benchmark(lambda x, y: x**2 + np.sin(2*np.pi*y), func_name="x^2+sin(2piy)", trials=20)
 
     # 3-layer functions
     # bench.benchmark(lambda x, y, z: (x+y*z)**3, func_name="(x+yz)^3", trials=20)
